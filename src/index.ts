27824841e57be6c9be3368daaafda096e0cf35d8
Labaile <hello@labaile.com>
2022-08-15 13:16:50 -0400

    lab 1 complete

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 function prepend(numSpaces: number, inputString: string) {
   // your code here
+let aString:string = "";
+
+  for(let i = 0; i<numSpaces;i++)
+  {
+    aString+=" ";
+  }
+  return aString.concat(inputString);
 }
+
+
+console.log(prepend(5,"Hello"));
+
